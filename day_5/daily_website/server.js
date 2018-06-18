c81ec1b24075c2d89a8bc6390c1ff3dd9dbc2339
Dan Oh <danoh82@gmail.com>
2018-06-17 18:34:23 -0700

    express added/setup, basic routing, viewersj WIP

---
+++ day_5/daily_website/server.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+let PORT = 3333 || process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static(__dirname + '/node_modules/viewerjs/dist'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res)=>{
+    res.render('index.ejs');
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Welcome to port ${PORT}`)
+})
