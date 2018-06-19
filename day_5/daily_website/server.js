a5708ab5e35b6526136a8e93dd5d46e497118d1d
Dan Oh <danoh82@gmail.com>
2018-06-18 22:52:08 -0700

    app icon design, dokdo site, map/reduce/sort/filter practice

--- day_5/daily_website/server.js
+++ day_5/daily_website/server.js
@@ -11,6 +11,10 @@ app.get('/', (req, res)=>{
     res.render('index.ejs');
 })
 
+app.get('/about', (req, res)=>{
+    res.render('history.ejs')
+})
+
 app.listen(PORT, ()=>{
     console.log(`Welcome to port ${PORT}`)
 })
